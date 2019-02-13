3846374c13cebb02c044c6d7ced754a07967e44c
Daniel Medina <isc.daniel.medina@gmail.com>
2019-02-13 15:18:49 -0800

    Añade comentarios a la evaluacion

--- evaluacion/evaluacion.scala
+++ evaluacion/evaluacion.scala
@@ -10,39 +10,52 @@ object Evaluacion {
         println(equilibrio(List(7,3,4)))
     }
 
+    // Verifica si un número es Par
     def esPar(num: Int): Boolean = if(num % 2 == 0) true else false
 
+    // Busca números pares en una lista
     def hayPares(lista: List[Int]): Boolean = {
+        // Itera la lista
         for(x <- lista){
+            // Revisa si el numero actual es par, si es así, devuelve verdadero y deja de iterar.
             if(esPar(x)){
                 return true
                 break
             }
         }
+        // Si no encuentra un número par devuelve falso
         false
     }
 
+    // Revisa si la suma de la mitad de la lista es igual a la suma de la otra mitad
     def equilibrio(lista: List[Int]): Boolean = {
         val index = lista.length/2
 
+        // Obtiene dos listas a partir de la original
         val mitad1 = lista.slice(0, index)
         val mitad2 = lista.slice(index, lista.length)
 
+        // Revisa si la suma es igual
         if(mitad1.sum == mitad2.sum) true else false
     }
 
     def afortunadoSiete(lista: List[Int]): Int = {
         var suma = 0
 
+        // Itera la lista buscando sietes, si encuentra uno suma 14, si no suma el numero original.
         for(x <- lista) if(x == 7) suma += 14 else suma += x
 
+        // Devuelve el resultado
         suma
     }
 
 
 
     def esPalindromo(cadena: String): Boolean = {
+        // Elimina los espacios de la cadena original y la converte a minúsculas
         val cadenaLimpia = cadena.replaceAll(" ", "").toLowerCase()
+
+        // Revisa si la cadena es igual inversamente
         if(cadenaLimpia == cadenaLimpia.reverse) true else false
     }
 }
